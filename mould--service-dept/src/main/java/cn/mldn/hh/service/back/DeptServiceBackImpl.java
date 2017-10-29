847ded5ce66805e92f5727ef6d7cd96bb3b96750
hh <13096947868@163.com>
2017-10-29 19:32:45 +0800

    SSM和Mybatis整合模板

---
+++ mould--service-dept/src/main/java/cn/mldn/hh/service/back/DeptServiceBackImpl.java
@@ -0,0 +1,40 @@
+package cn.mldn.hh.service.back;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import cn.mldn.hh.mould.service.back.IDeptServiceBack;
+import cn.mldn.hh.mould.vo.Dept;
+
+@Service 
+public class DeptServiceBackImpl implements IDeptServiceBack {
+
+	@Override
+	public boolean add(Dept vo) {
+		System.err.println("1、【IDeptSericeBack】增加新的部门数据：" + vo);
+		return true;
+	}
+
+	@Override
+	public List<Dept> list() {
+		System.err.println("2、【IDeptSericeBack】查询全部部门数据");
+		List<Dept> allDepts = new ArrayList<Dept>();
+		for (int x = 0; x < 10; x++) {
+			Dept vo = new Dept();
+			vo.setDeptno((long) x);
+			vo.setDname("mldn - " + x);
+			vo.setLoc("北京 - " + x);
+			allDepts.add(vo) ;
+		} 
+		return allDepts;
+	}
+
+	@Override
+	public boolean remove(long deptno) {
+		System.err.println("3、【IDeptSericeBack】删除部门数据，删除的部门编号：" + deptno);
+		return true; 
+	}
+
+}
---
+++ mould--service-member/src/main/java/cn/mldn/hh/mould/dao/IMemberDAO.java
@@ -0,0 +1,7 @@
+package cn.mldn.hh.mould.dao;
+
+import cn.mldn.hh.mould.vo.Member;
+
+public interface IMemberDAO { 
+	public Member findById(String id) ;
+}
